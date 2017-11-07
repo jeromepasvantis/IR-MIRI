36e2bfbf431123020b670233676c8d1d68365db8
jeromepasvantis <jerome.pasvantis@gmail.com>
2017-11-07 18:09:43 +0100

    Lab 3 first steps
    Implemented readRoutes and defined data structure Edges

---
+++ lab3/PageRank.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+from collections import namedtuple
+import time
+import sys
+
+class Edge:
+    def __init__ (self, origin=None, destination=None):
+        self.origin = origin 
+        self.destination = destination
+        self.weight = 1 
+
+    def __repr__(self):
+        return "edge: {0} {1} {2}".format(self.origin, self.destination, self.weight)
+
+
+    def increaseWeight(self):
+        self.weight = self.weight + 1
+        
+    ## write rest of code that you need for this class
+
+class Airport:
+    def __init__ (self, iden=None, name=None):
+        self.code = iden
+        self.name = name
+        self.routes = []
+        self.routeHash = dict()
+        self.outweight = 0
+        self.pageIndex = 0 # I don't know....
+
+    def __repr__(self):
+        #return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
+        return "{0}\t{1}\t{2}".format(self.code, self.name, self.outweight)
+
+edgeList = [] # list of Edge
+edgeHash = dict() # hash of edge to ease the match (key: IATA1 concat IATA2)
+edgeHash2 = dict() # hash key: origin -> List of edges
+airportList = [] # list of Airport
+airportHash = dict() # hash key IATA code -> Airport
+
+def readAirports(fd):
+    print "Reading Airport file from {0}".format(fd)
+    airportsTxt = open(fd, "r");
+    cont = 0
+    for line in airportsTxt.readlines():
+        a = Airport()
+        try:
+            temp = line.split(',')
+            if len(temp[4]) != 5 :
+                raise Exception('not an IATA code')
+            a.name=temp[1][1:-1] + ", " + temp[3][1:-1]
+            a.code=temp[4][1:-1]
+        except Exception as inst:
+            pass
+        else:
+            cont += 1
+            airportList.append(a)
+            airportHash[a.code] = a
+    airportsTxt.close()
+    print "There were {0} Airports with IATA code".format(cont)
+
+
+def readRoutes(fd):
+    print "Reading Routes file from {0}".format(fd)
+    routesTxt = open(fd, "r");
+    countDifferent = 0
+    countTotal = 0
+    for line in routesTxt.readlines():
+        try:
+            countTotal += 1
+            temp = line.split(',')
+            if (len(temp[2]) != 3 and len(temp[4]) != 3) :
+                raise Exception('not an IATA code')
+            # Extract i and j
+            origin = temp[2]
+            destination = temp[4]
+            # Increase Out(i)
+            airportHash[origin].outweight += 1
+            # Check if Route already in Edges
+            if origin+destination in edgeHash:
+                # Just increase the weight
+                edgeHash[origin+destination].increaseWeight()
+            else:
+                # Add new edge
+                countDifferent += 1
+                e = Edge(origin, destination)
+                edgeList.append(e)
+                edgeHash[origin+destination] = e
+                # Maintain second dict
+                if origin in edgeHash2:
+                    edgeHash2[origin].append(e)
+                else:
+                    edgeHash2[origin] = [e]
+        except Exception as inst:
+            pass
+    routesTxt.close()
+    print "There were {0} different routes out of {1} in total".format(countDifferent, countTotal)
+
+def computePageRanks():
+    print ""
+    # write your code
+
+def outputPageRanks():
+    print""
+    # write your code
+
+def main(argv=None):
+    readAirports("airports.txt")
+    readRoutes("routes.txt")
+
+    print airportHash["WYA"]
+    print edgeHash["WYAADL"]
+
+    print edgeHash2["FRA"][0]
+
+    time1 = time.time()
+    iterations = computePageRanks()
+    time2 = time.time()
+    outputPageRanks()
+    print "#Iterations:", iterations
+    print "Time of computePageRanks():", time2-time1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
